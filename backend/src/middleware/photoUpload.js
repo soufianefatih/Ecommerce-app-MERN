bbb84a43071dc135efb55dc197c436b35c8c9d1d
soufiane fatih <fatihe.soufiane@gmail.com>
2024-01-16 22:11:55 +0100

    ulpoad photo middleware

--- backend/src/middleware/photoUpload.js
+++ backend/src/middleware/photoUpload.js
@@ -30,5 +30,7 @@ const photoUpload = multer ({
         }else {
             cb({message :"Unsupported file format"},false)
         }
-    }
+    },
+    limits : {fileSize: 1024 * 1024}  // 1 meagbyte
 })
+module.exports = photoUpload
