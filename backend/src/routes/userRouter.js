f31182652ceb1032fe72d6998621b57badcf4e6e
soufiane fatih <fatihe.soufiane@gmail.com>
2024-01-18 23:45:05 +0100

    add controller user checkEmailExistence

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authController = require('../controllers/authController')
+const checkEmailExistence = require("../validator/checkEmailExistence")
 
 
 
@@ -7,7 +8,7 @@ const route = express.Router();
 
 route 
     .route('/register')
-    .post(authController.register)
+    .post(checkEmailExistence,authController.register)
 
 
 
