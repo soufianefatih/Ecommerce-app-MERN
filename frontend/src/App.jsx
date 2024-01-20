cf722eb19baa93819349045e87128f505e9a97b8
soufiane fatih <fatihe.soufiane@gmail.com>
2024-01-20 19:24:45 +0100

    addcart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <Header/>
     <Routes>
       <Route path="/" element ={<Home/>} />
+      <Route path="/products/category/:category_id" element={<Home />} />
     </Routes>
   
     </BrowserRouter>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,36 +1,39 @@
-/* eslint-disable react/jsx-no-undef */
 import React, { useEffect, useState } from 'react'
 import ProductListItem from './products/ProductListItem'
-import axios from 'axios';
+import axios from 'axios'
 import Categories from './categories/Categories';
+import { useParams } from 'react-router-dom';
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
+    const[products, setProducts] = useState([]);
+    const { category_id } = useParams()
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+               if(category_id) {
+                    const response = await axios.get(`http://localhost:5050/v1/product/category/${category_id}`);
+                    setProducts(response.data.result);
+               }else {
+                    const response = await axios.get('http://localhost:5050/v1/product');
+                    setProducts(response.data.result);
+               }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchProducts();
+    }, [category_id])
 
-   useEffect (()=>{
-    const fetchProducts = async ()=>{
-      try {
-        const response =await axios.get('http://localhost:5050/v1/product');
-         setProducts(response.data.result)
-         console.log(setProducts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-     fetchProducts();
-   },[])
-
-
-
-  return (
-    <div className='container'>
-      <Categories/>
-      <div className='row my-5'>
-           {
-             products?.map(product => <ProductListItem  key={product.id} product={product}/>)
-           }
-      </div>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <Categories />
+            <div className="row my-5">
+                {
+                    products?.map(product => <ProductListItem 
+                        key={product._id} product={product} />)
+                }
+            </div>
+        </div>
+    )
 }
