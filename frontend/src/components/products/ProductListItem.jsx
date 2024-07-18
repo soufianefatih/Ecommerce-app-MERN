3b14121a40b1029970e4cdeaeda5ad31e8e2ca45
soufiane fatih <fatihe.soufiane@gmail.com>
2024-07-18 22:28:17 +0100

    add new chages

--- frontend/src/components/products/ProductListItem.jsx
+++ frontend/src/components/products/ProductListItem.jsx
@@ -50,6 +50,7 @@ export default function ProductListItem({product, single}) {
                                 </div>
                             </div>
                         </div>
+                
                     </div>
                     
     
